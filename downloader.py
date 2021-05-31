1ccce233108dfee24c5bdd75843d04c45a12f260
evanwild <evan@evanwild.com>
2021-05-31 15:44:40 -0400

    Refactored code to be more clear

--- downloader.py
+++ downloader.py
@@ -12,110 +12,84 @@ API_CALLS_PER_MIN = 85
 # Some characters are not allowed in file names so they will be ignored
 EXCLUDE_CHARS = '\\/:*?"<>|'
 
-# Data included in the DMOJ download that tracks what problem each solution is for
-info = {}
 
-# The supplied directory which stores all the solution files
-solution_dir = ''
+class Solution:
+	def __init__(self, filepath, id_, extension, problem):
+		self.filepath = filepath
+		self.id_ = int(id_)
+		self.extension = extension
+		self.problem = problem
 
+	def process(self):
 
+		try:
+			url = f'https://dmoj.ca/api/problem/info/{self.problem}'
+			data = requests.get(url).json()
+			name = ''.join(c for c in data['name'] if c not in EXCLUDE_CHARS)
+			group = f'result/{data["group"]}'
 
-def get_problem_data(problem):
-	url = f'https://dmoj.ca/api/v2/problem/{problem}'
-	r = requests.get(url)
-	data = r.json()['data']['object']
-	return data
+			if not os.path.isdir(group):
+				os.mkdir(group)
+				print(f'INFO: Created {group} folder')
 
+			shutil.copyfile(self.filepath, f'{group}/{name}.{self.extension}')
 
-def make_category_path(category):
-	path = f'result/{category}'
-	if not os.path.exists(path):
-		os.mkdir(path)
-
-
-def process(problem, file):
-	try:
-		data = get_problem_data(problem)
-
-		name = data['name']
-		for char in EXCLUDE_CHARS:
-			name = name.replace(char, '')
-
-		category = data['group']
-		make_category_path(category)
-
-		ext = file.split('.')[1]
-
-		old_dir = f'{solution_dir}/{file}'
-		new_dir = f'result/{category}/{name}.{ext}'
-
-		shutil.copyfile(old_dir, new_dir)
-
-	except:
-		print(f'ERROR: No problem {problem}')
+		except:
+			print(f'ERROR: No problem exists with code {self.problem}')
 
 
-def is_latest(a, b):
-	a_num = int(a.split('.')[0])
-	b_num = int(b.split('.')[0])
-	return a_num > b_num
+def keep_newest(solutions):
+	result = []
+	seen = set()
+	solutions = sorted(solutions, key=lambda solution: solution.id_)
 
-
-def keep_most_recent(solutions):
-	result = {}
-	for solution in solutions:
-		if solution.name == 'info.json':
-			continue
-
-		num = solution.name.split('.')[0]
-		problem = info[num]['problem']
-
-		if problem not in result:
-			result[problem] = solution.name
-			continue
-
-		if is_latest(solution.name, result[problem]):
-			result[problem] = solution.name
+	for solution in reversed(solutions):
+		if solution.problem not in seen:
+			result.append(solution)
+			seen.add(solution.problem)
 
 	return result
 
 
 def main():
-	global info, solution_dir
 
-	try:
-		solution_dir = sys.argv[1]
-	except IndexError:
-		sys.exit('ERROR: Please specify the path to your solutions') 
+	if len(sys.argv) != 2:
+		sys.exit('ERROR: Please specify the directory to your solutions')
 
-	solutions = []
-	try:
-		solutions = os.scandir(solution_dir)
-	except FileNotFoundError:
+	old_dir = sys.argv[1]		
+
+	if not os.path.isdir(old_dir):
 		sys.exit('ERROR: That directory does not exist')
 
-	try:
-		info_file = open(f'{solution_dir}/info.json')
-		info = json.load(info_file)
-		info_file.close()
-	except FileNotFoundError:
-		sys.exit('ERROR: The info.json file does not exist in that directory')
-			
-	solutions = keep_most_recent(solutions)
-	
-	total = len(info)
-	unique = len(solutions)
-	print(f'INFO: Ignoring {total - unique} solutions that have newer versions')
+	filenames = os.listdir(old_dir)
+	filenames.remove('info.json')
 
-	if not os.path.exists('result'):
+	if not os.path.isfile(f'{old_dir}/info.json'):
+		sys.exit('ERROR: The info.json file does not exist in that directory')
+
+	info_file = open(f'{old_dir}/info.json')
+	info = json.load(info_file)
+	info_file.close()
+		
+	if not os.path.isdir('result'):
 		os.mkdir('result')
-		print(f'INFO: Created result folder')
+		print('INFO: Created results folder')
+
+	solutions = []
+	for filename in filenames:
+		filepath = f'{old_dir}/{filename}'
+		id_ = filename.split('.')[0]
+		extension = filename.split('.')[1]
+		problem = info[id_]['problem']
+		solutions.append(Solution(filepath, id_, extension, problem))
+
+	solutions = keep_newest(solutions)
 
 	done = 0
-	for problem, file in solutions.items():
-		process(problem, file)
+	for solution in solutions:
+		solution.process()
 		done += 1
-		print(f'INFO: Done {done}/{unique}')
+		print(f'INFO: Finished {solution.problem} - {done}/{len(solutions)}')
 		time.sleep(60 / API_CALLS_PER_MIN)
 
 
